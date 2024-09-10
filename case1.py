eb7eeb42910fc6a35d1893196b26bf60e3745260
Pavel <pavelpichugin28@gmail.com>
2024-09-10 23:44:14 +0200

    solved case1

--- case1.py
+++ case1.py
@@ -4,30 +4,40 @@
 Ответом на задачу будет ссылка на репозиторий GitHub, где хранится Ваша программа. Или иным удобным для Вас способом.
 """
 
-def find_summ_from_array(arr:list[int]) -> int:
+def find_summ_from_array(A:list[int]) -> int:
 
-    max, min = arr[0], arr[0]
+    max, min = A[0], A[0]
     max_index, min_index = 0, 0
 
-    for indx in range(len(arr)):
+    for indx in range(len(A)):
 
-        if arr[indx] > max:
+        if A[indx] > max:
             max_index = indx
+            max = A[indx]
 
-        if arr[indx] < min:
+        if A[indx] < min:
             min_index = indx
+            min = A[indx]
+
 
     if max_index<min_index:
         min_index, max_index = max_index, min_index
 
     res = 0
 
-    for indx in range(min_index, max_index):
-        if arr[indx]
+    for indx in range(min_index+1, max_index):
+        if A[indx] < 0:
+            res += A[indx]
+
+    return res
 
 
 
 if __name__ == "__main__":
-   res = find_summ_from_array([9, 3, 4,  1, -8, 7, -16, -1, -3, -19, 14])
+    try:
+        res = find_summ_from_array([9, 3, 4,  1, -8, 7, -16, -1, 4, 7, -1, -3, -11, 14])
 
-   assert res == -23
+        assert res == -16
+
+    except BaseException:
+        print("Произошла ошибка, проверьте свои вводные данные")
